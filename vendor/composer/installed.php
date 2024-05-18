7c3572daf2f1c2053ac9e193e8db6d75935c0c1c
vivihndr <vivihandari14@gmail.com>
2024-05-18 15:43:16 +0700

    [initcommit]

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'zam/simple-php-mvc-starter',
+        'pretty_version' => 'dev-Main',
+        'version' => 'dev-Main',
+        'reference' => 'a68e020819dfed8c0a22bfb04527c2f0ccf7c6d4',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'zam/simple-php-mvc-starter' => array(
+            'pretty_version' => 'dev-Main',
+            'version' => 'dev-Main',
+            'reference' => 'a68e020819dfed8c0a22bfb04527c2f0ccf7c6d4',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
